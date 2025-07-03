cb1bd2c504cd9a89f97fd26e03571aa0e70022d3
Nuth <nuthishere11@email.com>
2025-07-03 10:40:01 +0700

    Fix app.js and update tests to use ES modules

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+import express from 'express';
 
+const app = express();
+const port = 3000;
 
-const express = require('express'); 
-const app = express(); 
-const port = 3000; 
-app.get('/', (req, res) => { 
-res.send('Hello, GitHub Actions!'); 
-}); 
-app.listen(port, () => { 
-console.log(`App listening at http://localhost:${port}`); 
+app.get('/', (req, res) => {
+  res.send('Hello, GitHub Actions!');
 });
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+export default app;
